23b22bbbe32c23ca550176b9a96f1272e62880e0
Allan <kipropsallan@gmail.com>
2022-08-22 17:05:14 +0300

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+require 'pry'
 
+class Book
+    def initialize(title, author, pages)
+        @title = title
+        @author = author
+        @pages = pages
+    end
+    def title
+        @title
+    end
+    
 
