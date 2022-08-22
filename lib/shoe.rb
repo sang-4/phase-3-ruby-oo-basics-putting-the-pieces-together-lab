f02a54bf6f61e27817a86d10149c5a528be7df54
Allan <kipropsallan@gmail.com>
2022-08-22 17:51:59 +0300

    commit tree e58801ed5f34d236cc6850c5cb7cc1b6e8cef0a3

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,35 @@
+require 'pry'
+
 # Make your shoe class here!
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+  
+  def initialize(brand)
+    @brand = brand
+  end
+  def cobble 
+    @condition = "new"
+   puts "Your shoe is as good as new!"
+  end
+end
+
+    njumu = Shoe.new("Nike")
+    puts njumu.brand
+    
+    njumu.color = "red"
+    puts njumu.color
+
+    njumu.size = 9.5
+    puts njumu.size
+
+    njumu.material = "suede"
+    puts njumu.material
+
+    njumu.cobble
+    puts njumu.condition
+
+
+# binding.pry
+# 0
